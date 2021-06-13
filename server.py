98c2d2d01b687c0061911a210313ce89f5282520
hitesh.solanki <hitesh.solanki@games24x7.com>
2021-06-13 16:21:56 +0530

    created dockerfile

--- server.py
+++ server.py
@@ -51,4 +51,4 @@ class Places(Resource):
 api.add_resource(Places, '/v1')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='10.14.24.69', port=int(os.environ.get('PORT', 8082)))
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8082)))
