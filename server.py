269649a6fcae0249c205aa8fe00a152f4772c1d3
hitesh.solanki <hitesh.solanki@games24x7.com>
2021-06-14 01:04:47 +0530

    added v2 in dev

--- server.py
+++ server.py
@@ -6,32 +6,33 @@ import re
 app = Flask(__name__)
 api = Api(app)
 
-DATA = { 
- "version": "v1", 
+
+
+DATA2 = { 
+ "version": "v2", 
           "results": 
          [ 
-             "Machine1 had 0 attempt(s)",
+             "Machine1 - 0 time(s)",
          
-             "Machine2 had 0 attempt(s)",
+             "Machine2 - 0 time(s)",
          
-             "Machine3 had 0 attempt(s)"
+             "Machine3 - 0 time(s)"
          ]
     
 }
 
-
-class Places(Resource):
+class Places2(Resource):
     def get(self):
         # return our data and 200 OK HTTP code
-        return {'data': DATA}, 200
+        return {'data': DATA2}, 200
 
     def post(self):
         # parse request arguments
         parser = reqparse.RequestParser()
         parser.add_argument('m-location', required=True)
         args = parser.parse_args()
-        DATA['results'].append(args['m-location'])
-        return {'data': DATA}, 200
+        DATA2['results'].append(args['m-location'])
+        return {'data': DATA2}, 200
 
     def put(self):
         # parse request arguments
@@ -39,16 +40,16 @@ class Places(Resource):
         parser.add_argument('m-location')
         args = parser.parse_args()        
         if re.match("Machine1.*", args['m-location']):
-            DATA['results'][0] = args['m-location']
+            DATA2['results'][0] = args['m-location']
         if re.match("Machine2.*", args['m-location']):
-            DATA['results'][1] = args['m-location']
+            DATA2['results'][1] = args['m-location']
         if re.match("Machine3.*", args['m-location']):
-            DATA['results'][2] = args['m-location']
-        return {'data': DATA}, 200 
+            DATA2['results'][2] = args['m-location']
+        return {'data': DATA2}, 200 
 
 
 
-api.add_resource(Places, '/v1')
+api.add_resource(Places2, '/v2')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8082)))
+    app.run(debug=True, host='10.14.24.69', port=int(os.environ.get('PORT', 8082)))
